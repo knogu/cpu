9e9a9a732d14f53c924d0527e1e7a34daf796fef
knogu <kotaronoguchi61@gmail.com>
2025-05-03 16:24:46 +0900

    fix

--- TopModule.v
+++ TopModule.v
@@ -19,11 +19,16 @@ module TopModule(
 	);
 
     wire [31:0] a0;
-	m_proc3 m_cpu(CKL1, a0);
-    assign HEX0=a0[7:0];
-    assign HEX1=a0[15:8];
-    assign HEX2=a0[23:16];
-    assign HEX3=a0[31:24];
+	m_proc3 m_cpu(CLK1, a0);
+	wire [7:0] out1, out2, out3, out4;
+	m_seven_segment m1(a0[3:0], out1);
+	m_seven_segment m2(a0[7:4], out2);
+	m_seven_segment m3(a0[11:8], out3);
+	m_seven_segment m4(a0[15:12], out4);
+    assign HEX0=out1;
+    assign HEX1=out2;
+    assign HEX2=out3;
+    assign HEX3=out4;
 	
 endmodule
 
