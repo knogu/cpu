bef716a98495c745e8c3f39548acfd115c71cf10
knogu <kotaronoguchi61@gmail.com>
2025-05-03 16:41:39 +0900

    test2

--- cpu2.v
+++ cpu2.v
@@ -13,7 +13,7 @@ endmodule
 module m_am_imem(w_pc, w_insn);
   input  wire [31:0] w_pc;
   output wire [31:0] w_insn;
-  reg [31:0] mem [0:63]; /* synthesis ram_init_file = "imem.mif" */;
+  reg [31:0] mem [0:63]; /* synthesis ram_init_file = "imem.mif" */
   assign w_insn = mem[w_pc[7:2]];
 //   integer i; initial for (i=0; i<64; i=i+1) mem[i] = 32'd0;
 endmodule
