a069ea5bc2e91275f92fbf2a4346d0f9c4dfa8e6
knogu <kotaronoguchi61@gmail.com>
2025-05-03 16:55:11 +0900

    debug

--- TopModule.v
+++ TopModule.v
@@ -24,7 +24,7 @@ module TopModule(
 	m_seven_segment m1(a0[3:0], out1);
 	m_seven_segment m2(a0[7:4], out2);
 	m_seven_segment m3(a0[11:8], out3);
-	m_seven_segment m4(a0[15:12], out4);
+	m_seven_segment m4(4'd1, out4);
     assign HEX0=out1;
     assign HEX1=out2;
     assign HEX2=out3;
